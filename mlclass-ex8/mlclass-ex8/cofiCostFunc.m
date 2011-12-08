21d3b047f1cf680a30b00ea710489f8f5066a232
mdlima <mdlima.github@mailnull.com>
2011-12-08 21:58:05 -0200

    Ex8 - Part 2

--- mlclass-ex8/mlclass-ex8/cofiCostFunc.m
+++ mlclass-ex8/mlclass-ex8/cofiCostFunc.m
@@ -11,7 +11,7 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
+
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -41,16 +41,18 @@ Theta_grad = zeros(size(Theta));
 %
 
 
+J = sum(sum( (( (X * Theta') - Y) .* R) .^ 2 ))/2;
+
+X_grad = (( (X * Theta') - Y) .* R) * Theta;
+Theta_grad = (( (X * Theta') - Y) .* R)' * X;
 
 
+% Regularization
+J = J + lambda * (Theta(:)' * Theta(:)) / 2;
+J = J + lambda * (X(:)' * X(:)) / 2;
 
-
-
-
-
-
-
-
+X_grad = X_grad + lambda * X;
+Theta_grad = Theta_grad + lambda * Theta;
 
 
 
